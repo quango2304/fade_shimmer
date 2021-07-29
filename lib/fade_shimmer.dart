fa413091852d72de0194f17f982b377e4c31c169
aaroluo <aaro.luo@gmail.com>
2021-07-29 12:29:20 +0300

    removed FadeTheme

--- lib/fade_shimmer.dart
+++ lib/fade_shimmer.dart
@@ -7,8 +7,6 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
-enum FadeTheme { light, dark }
-
 class FadeShimmer extends StatefulWidget {
   final Color? highlightColor;
   final Color? baseColor;
@@ -16,40 +14,31 @@ class FadeShimmer extends StatefulWidget {
   final double width;
   final double height;
 
-  /// light or dark with predefined highlightColor and baseColor
-  /// need to pass this or highlightColor and baseColor
-  final FadeTheme? fadeTheme;
-
-  /// delay time before update the color, use this to make loading items animate follow each other instead of parallel, check the example for demo.
+  //delay time before update the color, use this to make loading items animate follow each other instead of parallel, check the example for demo.
   final int millisecondsDelay;
 
   const FadeShimmer(
       {Key? key,
       this.millisecondsDelay = 0,
       this.radius = 0,
-      this.fadeTheme,
       this.highlightColor,
       this.baseColor,
       required this.width,
       required this.height})
-      : assert(
-            (highlightColor != null && baseColor != null) || fadeTheme != null),
-        super(key: key);
+      : super(key: key);
 
-  /// use this to create a round loading widget
-  factory FadeShimmer.round(
-          {required double size,
-          Color? highlightColor,
-          int millisecondsDelay = 0,
-          Color? baseColor,
-          FadeTheme? fadeTheme}) =>
+  factory FadeShimmer.round({
+    required double size,
+    Color? highlightColor,
+    int millisecondsDelay = 0,
+    Color? baseColor,
+  }) =>
       FadeShimmer(
         height: size,
         width: size,
         radius: size / 2,
         baseColor: baseColor,
         highlightColor: highlightColor,
-        fadeTheme: fadeTheme,
         millisecondsDelay: millisecondsDelay,
       );
 
@@ -64,32 +53,22 @@ class _FadeShimmerState extends State<FadeShimmer> {
   bool isHighLight = true;
   late StreamSubscription sub;
 
-  Color get highLightColor {
-    if (widget.fadeTheme != null) {
-      switch (widget.fadeTheme) {
-        case FadeTheme.light:
-          return Color(0xffF9F9FB);
-        case FadeTheme.dark:
-          return Color(0xff3A3E3F);
-        default:
-          return Color(0xff3A3E3F);
-      }
+  Color get themeHighLightColor {
+    switch (Theme.of(context).brightness) {
+      case Brightness.light:
+        return Color(0xffF9F9FB);
+      case Brightness.dark:
+        return Color(0xff3A3E3F);
     }
-    return widget.highlightColor!;
   }
 
-  Color get baseColor {
-    if (widget.fadeTheme != null) {
-      switch (widget.fadeTheme) {
-        case FadeTheme.light:
-          return Color(0xffE6E8EB);
-        case FadeTheme.dark:
-          return Color(0xff2A2C2E);
-        default:
-          return Color(0xff2A2C2E);
-      }
+  Color get themeBaseColor {
+    switch (Theme.of(context).brightness) {
+      case Brightness.light:
+        return Color(0xffE6E8EB);
+      case Brightness.dark:
+        return Color(0xff2A2C2E);
     }
-    return widget.baseColor!;
   }
 
   @override
@@ -128,7 +107,9 @@ class _FadeShimmerState extends State<FadeShimmer> {
       width: widget.width,
       height: widget.height,
       decoration: BoxDecoration(
-          color: isHighLight ? highLightColor : baseColor,
+          color: isHighLight
+              ? widget.highlightColor ?? themeHighLightColor
+              : widget.baseColor ?? themeBaseColor,
           borderRadius: BorderRadius.circular(widget.radius)),
     );
   }
